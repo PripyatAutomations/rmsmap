04bf4246bce4c018a0c01608abffb7844acf45ea
Pripyat Automations <84538339+PripyatAutomations@users.noreply.github.com>
2022-11-01 02:03:53 -0400

    map enhancements

--- js/rmsmap.js
+++ js/rmsmap.js
@@ -29,10 +29,47 @@ $(document).ready(function() {
    $('div#links').hide();
 
    // leaflet setup
-   var mapOptions = { center: center_usa, zoom: 4 }
+   var mapOptions = {
+      center: center_usa,
+      zoom: 4,
+   }
    map = new L.map('rms-map', mapOptions);
-   base_layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+   base_layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 4,
+      maxZoom: 14
+   });
    map.addLayer(base_layer);
+
+   var graphicScale = L.control.graphicScale({
+      fill: 'fill',
+      doubleLine: true,
+      showSubunits: true
+   }).addTo(map);
+
+   let polylineMeasure = L.control.polylineMeasure ({position:'topleft', unit:'kilometres', showBearings:true, clearMeasurementsOnStop: false, showClearControl: true, showUnitControl: true})
+   polylineMeasure.addTo (map);
+
+   function debugevent(e) { console.debug(e.type, e, polylineMeasure._currentLine) }
+
+   map.on('polylinemeasure:toggle', debugevent);
+   map.on('polylinemeasure:start', debugevent);
+   map.on('polylinemeasure:resume', debugevent);
+   map.on('polylinemeasure:finish', debugevent);
+   map.on('polylinemeasure:change', debugevent);
+   map.on('polylinemeasure:clear', debugevent);
+   map.on('polylinemeasure:add', debugevent);
+   map.on('polylinemeasure:insert', debugevent);
+   map.on('polylinemeasure:move', debugevent);
+   map.on('polylinemeasure:remove', debugevent);
+
+   L.control.mouseCoordinate({
+      gps: true,
+      gpsLong: false,
+      nac: false,
+      qth: true,
+      utm: false,
+      utmref: true
+   }).addTo(map);
 
 //  ionicons webfont
 //   L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
@@ -54,10 +91,16 @@ $(document).ready(function() {
       update_kml(kml_file, map);
    });
 
+/* XXX: make this reset the page zoom on leaving a field
+   $('input').blur(function() {
+   });
+*/
    var kml_file = 'rms-' + $('#mode').val() + '.kml';
    update_kml(kml_file, map);
    // scale the map to fit all nodes
    // XXX: make this a toggle
 //   const bounds = activekmlLayer.getBounds();
 //   map.fitBounds(bounds);
+
+
 });
