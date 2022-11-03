5b97ccb575ed829cad1644b164b10a0712d97a9e
Pripyat Automations <84538339+PripyatAutomations@users.noreply.github.com>
2022-11-03 09:04:58 -0400

    Cleanup for layer settings (see menu).

--- js/rmsmap.js
+++ js/rmsmap.js
@@ -5,10 +5,13 @@ var ss = sessionStorage;
 var ls = localStorage
 var center_usa = [39.83333, -98.585522];
 var map;
+var tileUrl;
+var tileOptions = { };
+var magTileOptions = { };
+
+// layer cache
 var layers = [ ];
 var last_layer = 0;
-var scale_bar;
-
 var activekmlLayer = "";
 
 var mapOptions = {
@@ -19,10 +22,15 @@ var mapOptions = {
    zoomControl: false		// so we can use extended zoom controls
 };
 
-var config = {
+// increment if you change the configuration significantly
+var config_version = 1;
+var factory_config = {
+   auto_zoom: false,
+   bands: [ ],
    basemap: 'otm',
    help_at_start: false,
    mag_show_markers: false,
+   mode: 'rms-ardop',
    my_qth: null,
    map_center: center_usa,
    show_edge_markers: false,
@@ -30,80 +38,112 @@ var config = {
    show_tz: true,
    units: 'landmiles'
 };
+var config = factory_config;
 
 // Togglable things
 var edgeMarkerLayer;
+var polylineMeasure;
 var lit_earth;
 var time_zones;
+var scale_bar;
+var coordinates;
+var magnifyingGlass;
 
 ////////////////////////////////////////////////
+
+// Load factory settings
+function factory_reset() {
+   var do_reset = confirm("Are you sure you want to factory reset?");
+   
+   if (do_reset) {
+      console.log("* factory reset CONFIRMED");
+      config = factory_config;
+   } else
+      console.log("* factory reset CANCELLED");
+
+   update_settings_html();
+}
+
+// Load settings from local storage
+function load_settings() {
+   var lstmp;
+   var need_upgrade = false;
+
+   if ((lstmp = ls.getItem('myconfig_version')) !== null) {
+      if (config_version > lstmp) {
+         need_upgrade = true;
+      }
+   }
+
+   if ((lstmp = ls.getItem('myconfig')) !== null) {
+      // Decode it
+      var pc = JSON.parse(lstmp);
+
+      if (pc !== null && pc !== undefined) {
+         config = pc;
+         console.log("* load_settings from localStorage success");
+      } else {
+         console.log("* invalid localStorage configuration, ignoring");
+      }
+   } else {
+     console.log("* localStorage.myconfig not found, not loading");
+   }
+   update_settings_html();
+
+   if (need_upgrade) {
+      console("* upgrading saved configuration (v" + lstmp + ") to latest (v" + config_version + ")");
+      save_settings();
+   }
+}
+
+function save_settings() {
+   var res = ls.setItem('myconfig', JSON.stringify(config));
+   ls.setItem('myconfig_version', config_version);
+   console.log("* save_settings to localStorage");
+}
 
 ///////////////////////
 // Refresh statusbar //
 ///////////////////////
 function update_statusbar() {
    // Text strings
-   $('#t_band').text($('select#band').val());
+   // XXX: this will need exploded with commas?
+   $('#t_bands').text($('select#bands').val());
    $('#t_basemap').text($('select#basemap').val());
    $('#t_mode').text($('select#mode').val());
    $('#t_zoom').text(map.getZoom());
-}
-
-// Load settings from local storage
-function load_settings() {
-   /* load setting from localStorage if available */
-   var lstmp;
-
-   if ((lstmp = ls.getItem('basemap')) !== null) {
-      config.basemap = lstmp;
-   }
-
-   if ((lstmp = ls.getItem('help_at_start')) !== null) {
-      if (lstmp == false) {
-         config.help_at_start = false;
-      } else {
-         config.help_at_start = true;
-      }
-   }
-
-   if ((lstmp = ls.getItem('mag_show_markers')) !== null) {
-      if (lstmp == false) {
-         config.mag_show_markers = false;
-      } else {
-         config.mag_show_markers = true;
-         $('div#help').show();
-      }
-   }
-
-   if ((lstmp = ls.getItem('my_qth')) !== null) {
-      config.my_qth = lstmp;
-   }
-
-   if ((lstmp = ls.getItem('map_center')) !== null) {
-      config.map_center = lstmp;
-   }
-
-   if ((lstmp = ls.getItem('show_edge_markers')) !== null) {
-      config.show_edge_markers = lstmp;
-   }
-
-   if ((lstmp = ls.getItem('show_lit_earth')) !== null) {
-      config.show_lit_earth = lstmp;
-   }
-
-   if ((lstmp = ls.getItem('show_tz')) !== null) {
-      config.show_tz = lstmp;
+   if (config.auto_zoom) {
+      $('#t_auto_zoom').text("(auto)");
+   } else {
+      $('#t_auto_zoom').text('');
    }
 }
 
+// Set the html entities to reflect the loaded configuration
+// this should match the HTML
+function update_settings_html() {
+   $('input#auto_zoom').checked = config.auto_zoom;
+   $('select#bands').val(config.bands);
+   $('select#basemap').val(config.basemap);
+   $('input#help_at_start').val(config.help_at_start);
+   $('input#mag_show_markers').val(config.mag_show_markers);
+   $('input#mode').val(config.mode);
+   $('input#my_qth').val(config.my_qth);
+   $('input#show_edge_markers').val(config.show_edge_markers);
+   $('input#show_lit_earth').val(config.show_lit_earth);
+   $('input#show_tz').val(config.show_tz);
+   console.debug("* finished loading settings, applying.");
+}
+     
 // change the active displayed mode
-function set_mode(mymode) {
+function change_layer(mymode) {
    // unload old layer
    if (activekmlLayer != "") {
       map.removeLayer(activekmlLayer);
       activekmlLayer = "";
    }
 
+   console.log("* switching active overlay layer to " + mymode);
    var kml_file = mymode + '.kml';
    find_or_load_layer(mymode, kml_file);
 }
@@ -111,31 +151,8 @@ function set_mode(mymode) {
 // Find a layer in the cache or add it
 function find_or_add_layer(name, layer) {
    var i, sz;
-
-   if (last_layer == 0)
-      sz = 0;
-   else
-      sz = last_layer - 1;
-
-   console.log("sz: " + sz + " last_layer: " + last_layer);
-   for (i = 0; i <= sz; i++) {
-      console.log("test i: " + i);
-      console.log(layers[i]);
-
-      if (layers[i] === undefined) {
-         console.log("skipping no li");
-         break;
-      }
-
-      if (layers[i].name === undefined) {
-         console.log("skipping no lin");
-         break;
-      }
-
-      if (layers[i].name.match(name)) {
-         console.log("* using cached layer " + name);
-         return layers[i].layer;
-      }
+   if (layers[name] !== undefined && layers[name] !== null) {
+      layer = layers[name];
    }
 
    // This lets us search for an existing layer without loading it
@@ -143,13 +160,11 @@ function find_or_add_layer(name, layer) {
       console.log("* cache miss for layer " + name);
       return null;
    }
-
-   console.log("* storing layer " + name + " as " + last_layer);
-   layers[last_layer] = {
+   console.log("* storing layer " + name + " in cache");
+   layers[name] = {
        layer: layer,
        name: name
    };
-   last_layer++;
    return layer;
 }
 
@@ -173,8 +188,8 @@ function map_append_layer(map, track) {
    activekmlLayer = track;
 
    // scale the map to fit all nodes
-   if ($('input#autozoom').prop('checked')) {
-      const bounds = activekmlLayer.getBounds();
+   if (config.auto_zoom) {
+      const bounds = track.getBounds();
       map.fitBounds(bounds);
    }
 }
@@ -200,7 +215,7 @@ function load_kml(name, kmltext) {
          map_append_layer(map, track);
       });
    } else {
-      track = layers[i].layer;
+      track = layers[name].layer;
       map_append_layer(map, track);
    }
    update_statusbar();
@@ -210,7 +225,7 @@ function load_kml(name, kmltext) {
 ////////////////////////////
 
 function toggle_edge_markers() {
-   if ($('input#edge_markers').prop('checked')) {
+   if ($('input#show_edge_markers').prop('checked')) {
       edgeMarkerLayer = L.edgeMarker({
          findEdge : function(map){
              return L.bounds([200,0],map.getSize());
@@ -224,14 +239,13 @@ function toggle_edge_markers() {
       }).addTo(map);
    } else {
       if (edgeMarkerLayer !== undefined) {
-//         map.removeLayer(edgeMarkerLayer);
          edgeMarkerLayer.destroy();
       }
    }
 }
 
 function toggle_lit_earth() {
-   if ($('input#lit_earth').prop('checked')) {
+   if ($('input#show_lit_earth').prop('checked')) {
       lit_earth = L.terminator();
       lit_earth.addTo(map);
    } else {
@@ -244,11 +258,16 @@ function toggle_lit_earth() {
 }
 
 function toggle_tz() {
-   if ($('input#time_zones').prop('checked')) {
+   if ($('input#show_tz').prop('checked')) {
       time_zones = L.timezones;
+
+      // XXX: We need to improve the popup, disabling for now
+/*
       time_zones.bindPopup(function (layer) {
          return layer.feature.properties.time_zone;
-      }).addTo(map);
+      });
+*/
+      time_zones.addTo(map);
    } else {
       if (time_zones !== undefined) {
          map.removeLayer(time_zones);
@@ -257,7 +276,108 @@ function toggle_tz() {
    }
 }
 
-function update_scalebar(unit) {
+function toggle_magnifier() {
+   if ($('input#show_magnifier').prop('checked')) {
+      if (magnifyingGlass == undefined || magnifyingGlass == null) {
+         magnifyingGlass = L.magnifyingGlass({
+            zoomOffset: 3,
+            layers: [
+              L.tileLayer(tileUrl, magTileOptions)
+            ]
+         });
+
+         // Handle left click (
+         magnifyingGlass.on('click', function(e) {
+            if (config.mag_show_markers == true) {
+               config.mag_show_markers = false;
+            } else {
+               config.mag_show_markers = true;
+            }
+         });
+
+         // XXX: Move this out to do_contextmenu()
+         map.on('contextmenu', function(e) {
+           if (map.hasLayer(magnifyingGlass)) {
+              map.removeLayer(magnifyingGlass);
+           } else {
+              if ($('input#show_magnifier').prop('checked')) {
+                 map.addLayer(magnifyingGlass);
+                 magnifyingGlass.setLatLng(e.latlng);
+              }
+           }
+         });
+      }
+   } else {
+      if (magnifyingGlass !== undefined && magnifyingGlass !== null) {
+         if (map.hasLayer(magnifyingGlass)) {
+            map.removeLayer(magnifyingGlass);
+         }
+      }
+   }
+}
+
+function toggle_autozoom() {
+   if ($('input#autozoom').prop('checked')) {
+      config.auto_zoom = true;
+   } else {
+      config.auto_zoom = false;
+   }
+}
+
+function toggle_coordinates() {
+   if ($('input#show_tz').prop('checked')) {
+      coordinates = L.control.mouseCoordinate({
+         gps: true,
+         gpsLong: false,
+         nac: false,
+         qth: true,
+         utm: false,
+         utmref: true
+      }).addTo(map);
+   } else {
+      if (coordinates !== undefined) {
+//         map.removeLayer(coordinates);
+         coordinates.remove();
+      }
+   }
+}
+
+///////////////
+// measuring //
+///////////////
+function toggle_measuring() {
+   if ($('input#show_measuring').prop('checked')) {
+      if (polylineMeasure === undefined || polylineMeasure === null) {
+         polylineMeasure = L.control.polylineMeasure ({
+            position:'topleft',
+            unit: config.units,
+            showBearings:true,
+            clearMeasurementsOnStop: false,
+            showClearControl: true,
+            showUnitControl: true
+         });
+         map.on('polylinemeasure:toggle', polylinemeasureDebugevent);
+         map.on('polylinemeasure:start', polylinemeasureDebugevent);
+         map.on('polylinemeasure:resume', polylinemeasureDebugevent);
+         map.on('polylinemeasure:finish', polylinemeasureDebugevent);
+         map.on('polylinemeasure:change', polylinemeasureDebugevent);
+         map.on('polylinemeasure:clear', polylinemeasureDebugevent);
+         map.on('polylinemeasure:add', polylinemeasureDebugevent);
+         map.on('polylinemeasure:insert', polylinemeasureDebugevent);
+         map.on('polylinemeasure:move', polylinemeasureDebugevent);
+         map.on('polylinemeasure:remove', polylinemeasureDebugevent);
+      }
+
+      if (!map.hasLayer(polylineMeasure))
+         polylineMeasure.addTo(map);
+   } else {
+//      if (polylineMeasure !== undefined && polylineMeasure !== null) {
+//         map.remove(polylineMeasure);
+//      }
+   }
+}
+
+function update_scale(unit) {
    if (!scale_bar === undefined) {
       map.removeLayer(scale_bar);
    }
@@ -280,121 +400,11 @@ function update_scalebar(unit) {
       alert("Unknown scale unit: " + unit + ", cannot setup scalebar");
 }
 
-/////////////////
-
-$(document).ready(function() {
-   load_settings();
-   map = new L.map('rms-map', mapOptions);
-
-   //////////
-   // menu //
-   //////////
-   $('a#toggle_menu').click(function(e) {
-      $('div#menu').toggle();
-      e.preventDefault();
-   });
-
-   /////////////////////
-   // basemap setting //
-   /////////////////////
-   $('select#basemap').change(function() {
-      var tv = $('select#basemap').val()
-      $('#t_basemap').text(tv);
-      alert("Switching basemaps is not yet supported");
-   });
-
-   ///////////////////
-   // mode selector //
-   ///////////////////
-   $('select#mode').change(function() {
-      set_mode($('select#mode').val());
-   });
-
-   ///////////////
-   // measuring //
-   ///////////////
-   let polylineMeasure = L.control.polylineMeasure ({position:'topleft', unit:'kilometres', showBearings:true, clearMeasurementsOnStop: false, showClearControl: true, showUnitControl: true})
-   polylineMeasure.addTo (map);
-
-   function debugevent(e) {
-      console.debug(e.type, e, polylineMeasure._currentLine)
-   }
-
-   map.on('polylinemeasure:toggle', debugevent);
-   map.on('polylinemeasure:start', debugevent);
-   map.on('polylinemeasure:resume', debugevent);
-   map.on('polylinemeasure:finish', debugevent);
-   map.on('polylinemeasure:change', debugevent);
-   map.on('polylinemeasure:clear', debugevent);
-   map.on('polylinemeasure:add', debugevent);
-   map.on('polylinemeasure:insert', debugevent);
-   map.on('polylinemeasure:move', debugevent);
-   map.on('polylinemeasure:remove', debugevent);
-
-   ///////////////
-   // Scale bar //
-   ///////////////
-   update_scalebar(config.units);
-
-   /////////////////////
-   // coordinates box //
-   /////////////////////
-   L.control.mouseCoordinate({
-      gps: true,
-      gpsLong: false,
-      nac: false,
-      qth: true,
-      utm: false,
-      utmref: true
-   }).addTo(map);
-
-   ////////////////
-   // EdgeMarker //
-   ////////////////
-   toggle_edge_markers();
-   $('input#edge_markers').change(function() {
-       toggle_edge_markers();
-   });
-
-   ///////////////
-   // day/night //
-   ///////////////
-   toggle_lit_earth();
-   $('input#lit_earth').change(function() {
-      toggle_lit_earth();
-   });
-
-   ///////////////
-   // timezones //
-   ///////////////
-   toggle_tz();
-   $('input#time_zones').change(function() {
-      toggle_tz();
-   });
-
-   /////////////
-   // zooming //
-   /////////////
-   var zoom_bar = new L.Control.ZoomBar({position: 'topright'}).addTo(map);
-   map.on('zoomend', function() {
-     var currentZoom = map.getZoom();
-     update_statusbar();
-     /* XXX: Scale the markers */
-   });
-
-   var basemap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-   });
-   basemap.addTo(map);
-
-   ///////////////
-   // magnifier //
-   ///////////////
-   var tileUrl;
-   var tileOptions = { };
-   var magTileOptions = { };
-
-   if (/*('#t_basemap').val.match('otm')*/ true) {
+function load_basemap() {
+/*
+   var bmi = $('input#basemap option:selected');
+   if (bmi.val().match('otm')) {
+*/
       tileUrl = 'https://b.tile.opentopomap.org/{z}/{x}/{y}.png';
       tileOptions = {
          attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
@@ -406,7 +416,8 @@ $(document).ready(function() {
          subdomains: '1234',
          maxZoom: 14
       };
-   } else if (('#t_basemap').val().match('osm')) {
+/*
+   } else if (bmi.val().match('osm')) {
       tileUrl = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png';
       tileOptions = {
          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -419,30 +430,93 @@ $(document).ready(function() {
          maxZoom: 14
       };
    }
-   var magnifyingGlass = L.magnifyingGlass({
-      zoomOffset: 3,
-      layers: [
-        L.tileLayer(tileUrl, magTileOptions)
-      ]
+*/
+}
+
+function polylinemeasureDebugevent(e) {
+   console.debug(e.type, e, polylineMeasure._currentLine)
+}
+
+/////////////////
+
+function update_all() {
+   toggle_coordinates();
+   toggle_edge_markers();
+   toggle_magnifier();
+   toggle_measuring();
+   toggle_lit_earth();
+   toggle_tz();
+   toggle_autozoom();
+   update_statusbar();
+}
+
+$(document).ready(function() {
+   load_settings();
+   map = new L.map('rms-map', mapOptions);
+
+   var basemap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+   });
+   basemap.addTo(map);
+
+   //////////
+   // menu //
+   //////////
+   $('a#toggle_menu').click(function(e) {
+      $('div#menu').toggle();
+      e.preventDefault();
    });
 
-   // Handle left click (
-   magnifyingGlass.on('click', function(e) {
-      if (config.mag_show_markers == true) {
-         config.mag_show_markers = false;
-      } else {
-         config.mag_show_markers = true;
-      }
+   $('input#save_settings').click(function(e) {
+      save_settings();
    });
 
-   // ...and reappear on right click
-   map.on('contextmenu', function(e) {
-     if (map.hasLayer(magnifyingGlass)) {
-        map.removeLayer(magnifyingGlass);
-     } else {
-        map.addLayer(magnifyingGlass);
-        magnifyingGlass.setLatLng(e.latlng);
-     }
+   $('input#factory_reset').click(function(e) {
+      factory_reset();
+   });
+
+   $('input#autozoom').click(function(e) {
+      toggle_autozoom();
+   });
+
+   $('select#basemap').change(function() {
+      alert("Switching basemaps is not yet supported");
+      var tv = $('select#basemap').val()
+      $('#t_basemap').text(tv);
+   });
+
+   $('select#mode').change(function() {
+      change_layer($('select#mode').val());
+   });
+
+   $('input#show_edge_markers').change(function() {
+      toggle_edge_markers();
+   });
+
+   $('input#show_lit_earth').change(function() {
+      toggle_lit_earth();
+   });
+
+   $('input#show_measuring').change(function() {
+      toggle_measuring();
+   });
+
+   $('input#show_tz').change(function() {
+      toggle_tz();
+   });
+
+   $('input#show_magnifier').change(function() {
+      toggle_magnifier();
+   });
+
+   /////////////
+   // zooming //
+   /////////////
+   var zoom_bar = new L.Control.ZoomBar({position: 'topright'}).addTo(map);
+   map.on('zoomend', function() {
+     var currentZoom = map.getZoom();
+     update_statusbar();
+     /* XXX: Scale the markers */
    });
 
    /////////////
@@ -472,6 +546,7 @@ $(document).ready(function() {
    //////////////////////
    // initalize things //
    //////////////////////
-   set_mode();
-   update_statusbar();
+   change_layer($('select#mode').val());
+   load_basemap();
+   update_all();
 });
